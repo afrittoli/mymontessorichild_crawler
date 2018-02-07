580d5bf3c68a0adb3d4619da465fdc9086b78364
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-02-07 15:55:07 +0000

    Add logging, factor image get_image

--- get_images.sh
+++ get_images.sh
@@ -10,12 +10,26 @@ if [ "$1" == "help" ]; then
     exit 0
 fi
 
+# Log something
+function log_nice {
+  echo "$(date +%Y%m%d) [$TYPE][$SIZE] $*"
+}
+
+log_nice "Generating content in $OUTPUT"
+
 # Get a Session Cookie
 MONTESSORI_DOMAIN=www.mymontessorichild.com
 MONTESSORI_URL=https://${MONTESSORI_DOMAIN}/parents
 MONTESSORI_USER=$(security find-internet-password -s $MONTESSORI_DOMAIN | sed -n 's/^.*"acct"<blob>="\(.*\)"$/\1/p')
 MONTESSORI_PASSWORD=$(security find-internet-password -g -s $MONTESSORI_DOMAIN -w)
 COOKIE_PHPSESSID=$(curl -s --include Set-Cookie ${MONTESSORI_URL}/index.php?action=login --data "un=${MONTESSORI_USER}" --data "pw=${MONTESSORI_PASSWORD}" | sed -n 's/^.*PHPSESSID=\(.*\); e.*$/\1/p' | tail -1)
+
+if [ -z $COOKIE_PHPSESSID ]; then
+  log_nice "Could not get a session ID" >&2
+  exit 2
+fi
+
+log_nice "Got a PHP Session ID: $COOKIE_PHPSESSID"
 
 # Setup variables
 FULL_URL=$MONTESSORI_URL'/data.php?mode=history&data='$TYPE
@@ -38,26 +52,31 @@ fi
 eval $WGET '$FULL_URL' -O- 2>/dev/null | sed -e "s/ type=\"activity\"//g" -e "s/image.php?diary=\([0-9]*\)\"/$IMAGE_BASE\/\1.jpg\" alt=\"\1\"/g" > /tmp/_${TYPE}.html
 cat /tmp/header.html_fragment /tmp/_${TYPE}.html footer.html_fragment | tidy -q -w 500 > $OUTPUT/$TARGET_BASE.html
 
+log_nice "HTML page in $OUTPUT/$TARGET_BASE.html"
+
+function get_image_if_new {
+  _img = $1
+  _img_url = $2
+  if [ ! -f $TARGET_BASE/${_img}.jpg ]; then
+      eval $WGET '${_img_url}' -O$OUTPUT/$IMAGE_BASE/${_img}.jpg &> /dev/null
+      log_nice "New image $OUTPUT/$IMAGE_BASE/${_img}.jpg"
+  fi
+}
+
 # Get all square images
 sed -n 's/^.*class="square".*alt="\([0-9]*\)".*$/\1/p' $TARGET_BASE.html | while read aa; do
     aa_IMAGE_URL="${IMAGE_URL}${aa}&w=${SIZE}&h=${SIZE}"
-    if [ ! -f $TARGET_BASE/${aa}.jpg ]; then
-        eval $WGET '$aa_IMAGE_URL' -O$OUTPUT/$IMAGE_BASE/${aa}.jpg &> /dev/null
-    fi
+    get_image_if_new "$aa" "$aa_IMAGE_URL"
 done
 
 # Get all portrait images
 sed -n 's/^.*class="portrait".*alt="\([0-9]*\)".*$/\1/p' $TARGET_BASE.html | while read aa; do
     aa_IMAGE_URL="${IMAGE_URL}${aa}&h=${SIZE}&w=${RESIZE_WIDE_SIZE}"
-    if [ ! -f $TARGET_BASE/${aa}.jpg ]; then
-        eval $WGET '$aa_IMAGE_URL' -O$OUTPUT/$IMAGE_BASE/${aa}.jpg &> /dev/null
-    fi
+    get_image_if_new "$aa" "$aa_IMAGE_URL"
 done
 
 # Get all landscape images
 sed -n 's/^.*class="landscape".*alt="\([0-9]*\)".*$/\1/p' $TARGET_BASE.html | while read aa; do
     aa_IMAGE_URL="${IMAGE_URL}${aa}&h=${RESIZE_WIDE_SIZE}&w=${SIZE}"
-    if [ ! -f $TARGET_BASE/${aa}.jpg ]; then
-        eval $WGET '$aa_IMAGE_URL' -O$OUTPUT/$IMAGE_BASE/${aa}.jpg &> /dev/null
-    fi
+    get_image_if_new "$aa" "$aa_IMAGE_URL"
 done
