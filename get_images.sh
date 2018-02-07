cde97444aff75b0abfbf28d27fbc81c0d07686c3
Andrea Frittoli <andrea.frittoli@gmail.com>
2018-02-07 14:06:42 +0000

    Make tidy a bit less quiet
    Drop the -e option. Make the domain easily replacable.

--- get_images.sh
+++ get_images.sh
@@ -10,16 +10,17 @@ if [ "$1" == "help" ]; then
 fi
 
 # Get a Session Cookie
-SBMS_URL=https://www.mymontessorichild.com/parents
-MONTESSORI_USER=$(security find-internet-password -s www.mymontessorichild.com | sed -n 's/^.*"acct"<blob>="\(.*\)"$/\1/p')
-MONTESSORI_PASSWORD=$(security find-internet-password -g -s www.mymontessorichild.com -w)
-COOKIE_PHPSESSID=$(curl -s --include Set-Cookie ${SBMS_URL}/index.php?action=login --data "un=${MONTESSORI_USER}" --data "pw=${MONTESSORI_PASSWORD}" | sed -n 's/^.*PHPSESSID=\(.*\); e.*$/\1/p' | tail -1)
+MONTESSORI_DOMAIN=www.mymontessorichild.com
+MONTESSORI_URL=https://${MONTESSORI_DOMAIN}/parents
+MONTESSORI_USER=$(security find-internet-password -s $MONTESSORI_DOMAIN | sed -n 's/^.*"acct"<blob>="\(.*\)"$/\1/p')
+MONTESSORI_PASSWORD=$(security find-internet-password -g -s $MONTESSORI_DOMAIN -w)
+COOKIE_PHPSESSID=$(curl -s --include Set-Cookie ${MONTESSORI_URL}/index.php?action=login --data "un=${MONTESSORI_USER}" --data "pw=${MONTESSORI_PASSWORD}" | sed -n 's/^.*PHPSESSID=\(.*\); e.*$/\1/p' | tail -1)
 
 # Setup variables
-FULL_URL=$SBMS_URL'/data.php?mode=history&data='$TYPE
-IMAGE_URL=$SBMS_URL'/image.php?diary='
+FULL_URL=$MONTESSORI_URL'/data.php?mode=history&data='$TYPE
+IMAGE_URL=$MONTESSORI_URL'/image.php?diary='
 WGET="wget --header 'Cookie: PHPSESSID=$COOKIE_PHPSESSID'"
-TARGET_BASE="$(date +%Y%m%d)_sbms_leo_${TYPE}_${SIZE}"
+TARGET_BASE="$(date +%Y%m%d)_montessori_${TYPE}_${SIZE}"
 IMAGE_BASE="images_${SIZE}"
 STD_SIZE=180
 STD_WIDE_SIZE=235
@@ -34,7 +35,7 @@ fi
 
 # Get the main content page and build the full page
 eval $WGET '$FULL_URL' -O- 2>/dev/null | sed -e "s/ type=\"activity\"//g" -e "s/image.php?diary=\([0-9]*\)\"/$IMAGE_BASE\/\1.jpg\" alt=\"\1\"/g" > /tmp/_${TYPE}.html
-cat /tmp/header.html_fragment /tmp/_${TYPE}.html footer.html_fragment | tidy -e -q -w 500 > $TARGET_BASE.html
+cat /tmp/header.html_fragment /tmp/_${TYPE}.html footer.html_fragment | tidy -q -w 500 > $TARGET_BASE.html
 
 # Get all square images
 sed -n 's/^.*class="square".*alt="\([0-9]*\)".*$/\1/p' $TARGET_BASE.html | while read aa; do
